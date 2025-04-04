0e46c9eff4de87387d23efccc88a269a8aa9eab7
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2025-04-04 13:30:33 +0530

    update

--- question2/src/components/NumberFetcher.js
+++ question2/src/components/NumberFetcher.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./NumberFetcher.css"; // Optional styling
+import "./NumberFetcher.css";
 
 const NumberFetcher = () => {
   const [numbers, setNumbers] = useState([]);
